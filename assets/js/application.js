335caf8b023fda79bf3c80436b6477c0ca45deeb
Virginia <vcdooley@gmail.com>
2017-01-04 14:31:42 +0000

    updated styling in Bootstrap Creative theme

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,8 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, assets/javascripts,
+// or any plugin's assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -15,4 +15,6 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require creative
+//= require bootstrap
 //= require_tree .
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-
-  root 'welcome#index'
-#defined route 'findmeetups' This named route goes to this controller#action
-  get 'findmeetups', to: 'welcome#search', as: :search
-end
